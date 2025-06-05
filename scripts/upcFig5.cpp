3b2d54883dd7bc7a535d9ebd5f8fab0a5e2432af
huinaibing <huinaibing@gmail.com>
2025-06-05 15:34:21 +0800

    finish the Recurrence of UPC fig5

--- scripts/upcFig5.cpp
+++ scripts/upcFig5.cpp
@@ -5,6 +5,7 @@
 #include "TProfile.h"
 #include "TStyle.h"
 #include "TGraphErrors.h"
+#include "TLegend.h"
 
 #define JPSI_SLIGHT_PATH "/home/huinaibing/huinaibing/PA/DATA_FILES/slight_out/Jpsislight.out"
 #define PSI2S_SLIGHT_PATH "/home/huinaibing/huinaibing/PA/DATA_FILES/slight_out/psi2Sslight.out"
@@ -78,10 +79,37 @@ void upc_fig5()
         jpsi2psi2Sratio->SetPoint(point_index, x, y);
         point_index++;
     }
-    TCanvas* canvas = new TCanvas("canvas", "Psi(2S) Ratio to J/psi", 800, 600);
+    // 就两个点，别开个新类了
+    double x1 = 932.5577811596276;
+    double x2 = 1999.3610734110384;
+    double y1 = 0.16512120054179386;
+    double y2 = 0.16507791375519917;
+
+    TGraphErrors* paper = new TGraphErrors(jpsi_valid_points < psi2s_valid_points ? jpsi_valid_points : psi2s_valid_points);
+    paper->SetPoint(0, x1, y1);
+    paper->SetPoint(1, x2, y2);
+    
+
+    TCanvas* canvas = new TCanvas("canvas", "Psi(2S) Ratio to J/psi", 1600, 1000);
+    TH1F* frame = new TH1F("frame", "Psi(2S) Ratio to J/psi", 100, 800, 3000);
+    frame->SetStats(0);
+    frame->GetYaxis()->SetTitle("Ratio");
+    frame->GetXaxis()->SetTitle("W_{#gamma p} (GeV)");
+    frame->Draw();
+
     jpsi2psi2Sratio->SetTitle("Psi(2S) to J/psi Ratio; W_{#gamma p} (GeV); Ratio");
     jpsi2psi2Sratio->SetMarkerStyle(20);
-    jpsi2psi2Sratio->Draw("AP");
+    jpsi2psi2Sratio->SetMarkerColor(kBlue);
+    jpsi2psi2Sratio->Draw("P SAME");
+
+    paper->SetMarkerStyle(23);
+    paper->SetMarkerColor(kRed);
+    paper->Draw("P SAME");
+
+    TLegend *legend = new TLegend(0.1, 0.7, 0.3, 0.9);
+    legend->AddEntry(jpsi2psi2Sratio, "my result", "p");
+    legend->AddEntry(paper, "Paper", "p");
+    legend->Draw();
+
     canvas->SaveAs("wgmp_cross_section/psi2S_jpsi_ratio.png");
 }
-    
